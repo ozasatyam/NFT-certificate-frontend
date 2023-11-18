0fcb16dd6ce80581688414f9f6c8eabdc347904e
ozasatyam <satyamoza@outlook.com>
2023-11-18 01:19:10 -0700

    contract connected

--- src/web3Provider/MetaConnect.js
+++ src/web3Provider/MetaConnect.js
@@ -1,20 +1,28 @@
 import { useSDK } from "@metamask/sdk-react";
-import { ethers } from "ethers";
+import { ethers, JsonRpcProvider } from "ethers";
 import React, { useEffect, useState } from "react";
 import Certificate from "../abis/Certificate.json"
 
 const MetaConnect = () => {
 	const { sdk, connected, connecting, provider, chainId } = useSDK();
 	const [account, setAccount] = useState();
+	const [contract, setContract] = useState();
+
+	const callContract = () => {
+		const provider = new JsonRpcProvider(process.env.REACT_APP_RPC_URL);
+		const signer = new ethers.Wallet(
+			process.env.REACT_APP_PRIVATE_KEY,
+			provider
+		);
+		const c = new ethers.Contract(process.env.REACT_APP_CONTRACT_ADDRESS, Certificate.abi, signer)
+		setContract(c)
+	}
 
 	const connect = async () => {
 		try {
 			const accounts = await sdk?.connect();
 			setAccount(accounts?.[0]);
-			console.log("contract create")
-			const contract = new ethers.Contract("0x17acd5d58aa1c0a54a5ee9a70eb0362d92098667", Certificate.abi)
-			console.log(contract)
-
+			callContract()
 		} catch (err) {
 			console.warn(`failed to connect..`, err);
 		}
@@ -50,69 +58,3 @@ const MetaConnect = () => {
 
 export default MetaConnect;
 
-// // import { PIXEL_AVATAR_NETWORK } from '../constants'
-
-// export default class MetaMask {
-// 	constructor() {
-// 		this.provider = window.ethereum;
-// 	}
-
-// 	async connect(triggerPrompt = false) {
-// 		if (triggerPrompt) {
-// 			await this.provider.request({
-// 				method: "wallet_requestPermissions",
-// 				params: [
-// 					{
-// 						eth_accounts: {},
-// 					},
-// 				],
-// 			});
-// 		}
-
-// 		const [address] = await this.provider.request({
-// 			method: "eth_requestAccounts",
-// 		});
-
-// 		return address;
-// 	}
-
-// 	async disconnect() {
-// 		//
-// 	}
-
-// 	// async changeNetwork() {
-// 	//     const chainId = '0x' + PIXEL_AVATAR_NETWORK.chainId.toString(16)
-
-// 	//     try {
-// 	//         await this.provider.request({
-// 	//             method: 'wallet_switchEthereumChain',
-// 	//             params: [
-// 	//                 {
-// 	//                     chainId: chainId,
-// 	//                 },
-// 	//             ],
-// 	//         })
-// 	//     } catch (switchError) {
-// 	//         await this.provider.request({
-// 	//             method: 'wallet_addEthereumChain',
-// 	//             params: [
-// 	//                 {
-// 	//                     chainId: chainId,
-// 	//                     chainName: PIXEL_AVATAR_NETWORK.name,
-// 	//                     rpcUrls: [PIXEL_AVATAR_NETWORK.ensAddress],
-// 	//                     nativeCurrency: {
-// 	//                         name: PIXEL_AVATAR_NETWORK.currencySymbol,
-// 	//                         symbol: PIXEL_AVATAR_NETWORK.currencySymbol,
-// 	//                         decimals: 18,
-// 	//                     },
-// 	//                     blockExplorerUrls: [PIXEL_AVATAR_NETWORK.blockExplorer],
-// 	//                 },
-// 	//             ],
-// 	//         })
-// 	//     }
-// 	// }
-
-// 	static isAvailable() {
-// 		return typeof window.ethereum !== "undefined";
-// 	}
-// }
